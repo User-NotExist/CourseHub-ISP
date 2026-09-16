b5dfdcc0286c5330a41bb7c71b558bcb39cb7a7b
User-NotExist <49120419+User-NotExist@users.noreply.github.com>
2026-09-16 17:34:26 +0700

    ui: created empty course page/moved layout

---
+++ source/coursehub/src/app/courses/[id]/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useState, useEffect} from "react"
+import CoursePage from "@/components/course"
+
+export default function InstanceCoursePage() {
+    return (
+        <div>
+            <CoursePage />
+        </div>
+    )
+}
