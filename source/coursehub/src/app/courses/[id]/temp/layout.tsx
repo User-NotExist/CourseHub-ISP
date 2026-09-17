b1037aa5b2ba5b6b04bf8fc49d5dedba298fdb30
User_NotExist <49120419+User-NotExist@users.noreply.github.com>
2026-09-17 19:07:25 +0700

    ui: created empty course page/moved layout (#30)

--- source/coursehub/src/app/courses/[id]/temp/layout.tsx
+++ source/coursehub/src/app/courses/[id]/temp/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Inria_Sans } from "next/font/google";
-import "../../globals.css";
+import "../../../globals.css";
 
 import Sidebar from "@/components/sidebar";
 
---
+++ source/coursehub/src/components/course.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function CoursePage() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            Course Main Page
+        </div>
+    )
+}
